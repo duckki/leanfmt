8288a95d2b5c398a32d289ae9b6277584ed97fb7
Duckki Oe <duckki.dev@gmail.com>
2026-08-15 14:17:10 -0700

    test: stabilize CI test harness

--- LeanFmt/Tests/Run.lean
+++ LeanFmt/Tests/Run.lean
@@ -1,7 +1,7 @@
 import LeanFmt.Tests.Suite
 
-def main (_args : List String) : IO UInt32 := do
+def main (args : List String) : IO UInt32 := do
   Lean.initSearchPath (← Lean.findSysroot)
   let env ← LeanFmt.Formatter.defaultEnvironment
-  LeanFmt.Tests.runTestGroups env
+  LeanFmt.Tests.runTestGroups env args
   pure 0
